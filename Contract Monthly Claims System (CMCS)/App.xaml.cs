b8b14720ae9edacd9d4f03e1e16370a3dc40f950
khutjo <Khutsomamabolo99@gmail.com>
2025-09-06 19:20:09 +0200

    Add project files.

---
+++ Contract Monthly Claims System (CMCS)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Contract_Monthly_Claims_System__CMCS_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
